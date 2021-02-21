d26c79d3c10a379a0e37dcffae2f3f1dd3ff9059
ungericw <ungericw@tutanota.com>
2021-02-20 20:43:52 -0600

    2/20/2021 added home controller

--- Startup.cs
+++ Startup.cs
@@ -45,7 +45,9 @@ namespace myWebApp
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
